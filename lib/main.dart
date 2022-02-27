b889b07763ef9454079396f63194bc8f7b4d32a9
June <jonasjzy@gmail.com>
2022-02-27 21:21:44 +0800

    :fire:

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: '贇 DunDun',
+      title: 'bing DunDun',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
